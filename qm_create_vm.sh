679561fac538cb3db67244604b995a3f12f3a2aa
Matheus Presotto <83012707+presotto-m@users.noreply.github.com>
2023-01-10 11:48:38 -0300

    Create qm_create_vm.sh

---
+++ qm_create_vm.sh
@@ -0,0 +1,13 @@
+# Download da imagem (Sua escolha) 
+wget https://cdimage.debian.org/cdimage/cloud/bullseye/20221219-1234/debian-11-genericcloud-amd64-20221219-1234.qcow2
+
+# Crie uma nova VM com o controlador VirtIO SCSI
+qm create 200 --name debian11-cloudinit --net0 virtio,bridge=vmbr2
+
+# Importe o disco baixado para o armazenamento local-lvm, anexando-o como uma unidade SCSI
+qm importdisk 200 ebian-11-genericcloud-amd64-20221219-1234.qcow2 local-lvm
+qm set 200 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-900-disk-0
+qm set 200 --ide2 local-lvm:cloudinit
+qm set 200 --boot c --bootdisk scsi0
+qm set 200 --serial0 socket --vga serial0
+qm template 200
